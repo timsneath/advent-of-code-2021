8219d53fe5c06c23d795aaf5ca9f98871b949523
Tim Sneath <tim@sneath.org>
2021-12-27 22:48:02 -0800

    Read boards

---
+++ day04/day04.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  final path = args.isNotEmpty ? args[0] : 'day03/day03.txt';
+  final rawData = File(path).readAsLinesSync();
+
+  // Read bingo numbers in order that they're called
+  final randomOrder = rawData[0].split(',').map((e) => int.parse(e));
+
+  // Read bingo boards
+  final boards = <List<List>>[[]];
+  for (var dataRow = 2; dataRow < rawData.length; dataRow += 6) {
+    final board = <List<int>>[[]];
+    for (var bingoRow = 0; bingoRow < 5; bingoRow++) {
+      final rawRow = rawData[dataRow + bingoRow];
+      board.add(rawRow.split(RegExp(r'\s+')).map((e) => int.parse(e)).toList());
+    }
+    boards.add(board);
+  }
+
+  print(boards.length);
+}
---
+++ test/day04_test.dart
@@ -0,0 +1,5 @@
+import 'package:test/test.dart';
+
+import '../day04/day04.dart';
+
+void main() {}
