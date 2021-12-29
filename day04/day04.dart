9cb58980241c2b8747f209066dd6e6c546822a92
Tim Sneath <tim@sneath.org>
2021-12-29 14:30:16 -0800

    Day04 (#3)

---
+++ day04/day04.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+
+extension Bingo on List<Board> {
+  /// Plays a round of bingo on a list of boards. Return the index of every
+  /// board that won on that round.
+  List<int> playRound(int number) {
+    var winningBoards = <int>[];
+    for (var idx = 0; idx < length; idx++) {
+      if (this[idx].drawNumber(number)) {
+        winningBoards.add(idx);
+      }
+    }
+    return winningBoards;
+  }
+
+  /// Plays multiple rounds of bingo on a list of boards. Return the index of every
+  /// board that won during those rounds, in order of victory.
+  List<int> playRounds(List<int> numbers) {
+    var winningBoards = <int>[];
+
+    for (final number in numbers) {
+      final result = playRound(number);
+      winningBoards.addAll(result);
+    }
+
+    return winningBoards;
+  }
+}
+
+class Board {
+  // Numbers in the board
+  late final List<int> board;
+
+  // Which board entries have been drawn so far
+  late final List<bool> drawn;
+
+  bool isVictory = false;
+
+  Board(List<List<int>> boardList) {
+    // Flatten board to a list of integers
+    board = [for (var row in boardList) ...row];
+    if (board.length != 25) {
+      throw ArgumentError('Board should be a 5x5 matrix');
+    }
+
+    resetGame();
+  }
+
+  void resetGame() {
+    drawn = List<bool>.generate(board.length, (e) => false);
+  }
+
+  bool checkVictory() {
+    // Check for a victory based on a row
+    for (var row = 0; row < 5; row++) {
+      if (drawn.getRange(row * 5, (row * 5) + 5).every((elem) => elem)) {
+        isVictory = true;
+        return true;
+      }
+    }
+
+    // Check for a victory based on a column
+    for (var i = 0; i < 5; i++) {
+      final column = [
+        drawn.elementAt(i),
+        drawn.elementAt(i + 5),
+        drawn.elementAt(i + 10),
+        drawn.elementAt(i + 15),
+        drawn.elementAt(i + 20),
+      ];
+
+      if (column.every((element) => element)) {
+        isVictory = true;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /// Returns true if this number leads to a new winning board.
+  bool drawNumber(int number) {
+    // Board was already victorious, so this isn't a new victory
+    if (isVictory) return false;
+
+    final indexOfNumber = board.indexOf(number);
+    if (indexOfNumber != -1) {
+      drawn[board.indexOf(number)] = true;
+      return checkVictory();
+    } else {
+      // Number wasn't found, so the board is no more "winning" than it was
+      // before.
+      return false;
+    }
+  }
+
+  int sumUnmarkedNumbers() {
+    var sum = 0;
+    for (var i = 0; i < drawn.length; i++) {
+      if (!drawn[i]) {
+        sum += board[i];
+      }
+    }
+
+    return sum;
+  }
+}
+
+void main(List<String> args) {
+  final path = args.isNotEmpty ? args[0] : 'day04/day04.txt';
+  final rawData = File(path).readAsLinesSync();
+
+  // Create a queue of bingo numbers in order that they're called
+  final calledNumbers =
+      rawData[0].split(',').map<int>((e) => int.parse(e)).toList();
+
+  // Read bingo boards
+  final boards = <Board>[];
+  for (var dataRow = 2; dataRow < rawData.length; dataRow += 6) {
+    final board = <List<int>>[];
+    for (var bingoRow = 0; bingoRow < 5; bingoRow++) {
+      final rawRow = rawData[dataRow + bingoRow].trimLeft();
+      board.add(rawRow.split(RegExp(r'\s+')).map((e) => int.parse(e)).toList());
+    }
+    boards.add(Board(board));
+  }
+
+  // Play bingo
+  var winningBoards = <int>[];
+  var winningScores = <int>[];
+  for (final calledNumber in calledNumbers) {
+    winningBoards = boards.playRound(calledNumber);
+    for (final board in winningBoards) {
+      final score = boards[board].sumUnmarkedNumbers() * calledNumber;
+      winningScores.add(score);
+    }
+  }
+
+  // Print results
+  if (winningScores.isEmpty) {
+    print('No winning board');
+  } else {
+    print('First winning score: ${winningScores.first}');
+    print('Last winning score: ${winningScores.last}');
+  }
+}
---
+++ test/day04_test.dart
@@ -0,0 +1,96 @@
+import 'package:test/test.dart';
+
+import '../day04/day04.dart';
+
+const randomOrder = [
+  // Numbers "drawn"
+  7, 4, 9, 5, 11, 17, 23, 2, 0,
+  14, 21, 24, 10, 16, 13, 6, 15,
+  25, 12, 22, 18, 20, 8, 19, 3, 26, 1
+];
+
+List<Board> initBoards() => [
+      Board([
+        [22, 13, 17, 11, 0],
+        [8, 2, 23, 4, 24],
+        [21, 9, 14, 16, 7],
+        [6, 10, 3, 18, 5],
+        [1, 12, 20, 15, 19],
+      ]),
+      Board([
+        [3, 15, 0, 2, 22],
+        [9, 18, 13, 17, 5],
+        [19, 8, 7, 25, 23],
+        [20, 11, 10, 24, 4],
+        [14, 21, 16, 12, 6],
+      ]),
+      Board([
+        [14, 21, 17, 24, 4],
+        [10, 16, 15, 9, 19],
+        [18, 8, 23, 26, 20],
+        [22, 11, 13, 6, 5],
+        [2, 0, 12, 3, 7]
+      ])
+    ];
+
+void main() {
+  test('Worked example on a single board', () {
+    final board = initBoards()[2];
+
+    int index = 0;
+    bool isVictory = false;
+
+    do {
+      isVictory = board.drawNumber(randomOrder[index++]);
+    } while (!isVictory && index < randomOrder.length);
+
+    expect(randomOrder[index - 1], equals(24));
+    expect(board.sumUnmarkedNumbers(), equals(188));
+  });
+
+  test('Worked example on all boards', () {
+    final boards = initBoards();
+    final winningBoard = boards.playRounds(randomOrder);
+    expect(winningBoard.first, equals(2));
+  });
+
+  test('Winning column', () {
+    final boards = initBoards();
+    final winningBoard = boards.playRounds([15, 18, 8, 11, 21]);
+    expect(winningBoard.first, equals(1));
+  });
+
+  test('Winning row', () {
+    final boards = initBoards();
+    final winningBoard = boards.playRounds([21, 9, 6, 14, 0, 16, 2, 7]);
+    expect(winningBoard.first, equals(0));
+  });
+
+  test('No winning board', () {
+    final boards = initBoards();
+    final winningBoard = boards.playRounds([15, 18, 8, 11, 22, 7]);
+    expect(winningBoard, isEmpty);
+  });
+
+  test('Boards win in order', () {
+    final boards = initBoards();
+    var winningBoard =
+        boards.playRounds([7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24]);
+    expect(winningBoard, equals([2]));
+
+    winningBoard = boards.playRound(10);
+    expect(winningBoard, isEmpty);
+    winningBoard = boards.playRound(16);
+    expect(winningBoard, equals([0]));
+
+    winningBoard = boards.playRound(13);
+    expect(winningBoard, equals([1]));
+    expect(boards[1].sumUnmarkedNumbers(), equals(148));
+  });
+
+  test('Play all', () {
+    final boards = initBoards();
+    var winningBoards = boards.playRounds(randomOrder);
+    expect(winningBoards, equals([2, 0, 1]));
+  });
+}
