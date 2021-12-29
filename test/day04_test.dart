8841b4d62d09a0ed671c88520d24b34a9074f495
Tim Sneath <tim@sneath.org>
2021-12-29 13:10:20 -0800

    Don't forget to reset boards in test!

--- test/day04_test.dart
+++ test/day04_test.dart
@@ -9,33 +9,33 @@ const randomOrder = [
   25, 12, 22, 18, 20, 8, 19, 3, 26, 1
 ];
 
-final boards = [
-  Board([
-    [22, 13, 17, 11, 0],
-    [8, 2, 23, 4, 24],
-    [21, 9, 14, 16, 7],
-    [6, 10, 3, 18, 5],
-    [1, 12, 20, 15, 19],
-  ]),
-  Board([
-    [3, 15, 0, 2, 22],
-    [9, 18, 13, 17, 5],
-    [19, 8, 7, 25, 23],
-    [20, 11, 10, 24, 4],
-    [14, 21, 16, 12, 6],
-  ]),
-  Board([
-    [14, 21, 17, 24, 4],
-    [10, 16, 15, 9, 19],
-    [18, 8, 23, 26, 20],
-    [22, 11, 13, 6, 5],
-    [2, 0, 12, 3, 7]
-  ])
-];
+List<Board> initBoards() => [
+      Board([
+        [22, 13, 17, 11, 0],
+        [8, 2, 23, 4, 24],
+        [21, 9, 14, 16, 7],
+        [6, 10, 3, 18, 5],
+        [1, 12, 20, 15, 19],
+      ]),
+      Board([
+        [3, 15, 0, 2, 22],
+        [9, 18, 13, 17, 5],
+        [19, 8, 7, 25, 23],
+        [20, 11, 10, 24, 4],
+        [14, 21, 16, 12, 6],
+      ]),
+      Board([
+        [14, 21, 17, 24, 4],
+        [10, 16, 15, 9, 19],
+        [18, 8, 23, 26, 20],
+        [22, 11, 13, 6, 5],
+        [2, 0, 12, 3, 7]
+      ])
+    ];
 
 void main() {
   test('Worked example on a single board', () {
-    final board = boards[2];
+    final board = initBoards()[2];
 
     int index = 0;
     bool isVictory = false;
@@ -49,21 +49,25 @@ void main() {
   });
 
   test('Worked example on all boards', () {
+    final boards = initBoards();
     final winningBoard = boards.playRounds(randomOrder);
     expect(winningBoard, equals(2));
   });
 
   test('Winning column', () {
+    final boards = initBoards();
     final winningBoard = boards.playRounds([15, 18, 8, 11, 21]);
     expect(winningBoard, equals(1));
   });
 
   test('Winning row', () {
+    final boards = initBoards();
     final winningBoard = boards.playRounds([21, 9, 6, 14, 0, 16, 2, 7]);
     expect(winningBoard, equals(0));
   });
 
   test('No winning board', () {
+    final boards = initBoards();
     final winningBoard = boards.playRounds([15, 18, 8, 11, 22, 7]);
     expect(winningBoard, equals(-1));
   });
