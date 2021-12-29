c29308897164576eaf2e742f5303963be10255d7
Tim Sneath <tim@sneath.org>
2021-12-29 10:51:57 -0800

    Add board logic

--- day04/day04.dart
+++ day04/day04.dart
@@ -1,5 +1,50 @@
 import 'dart:io';
 
+class Board {
+  late final List<int> board;
+  late final List<bool> drawn;
+
+  Board(List<List<int>> boardList) {
+    if (boardList.length != 25) {
+      throw ArgumentError('Board should be 5x5 cells');
+    }
+
+    board = [for (var row in boardList) ...row];
+    drawn = List<bool>.generate(board.length, (e) => false);
+  }
+
+  bool checkVictory() {
+    // Check for a victory based on a row
+    for (var row = 0; row < 5; row++) {
+      if (drawn
+          .getRange(row * 5, (row * 5) + 5)
+          .where((element) => element)
+          .isNotEmpty) return true;
+    }
+
+    // Check for a victory based on a column
+    for (var i = 0; i < 5; i++) {
+      final column = [
+        drawn.elementAt(i),
+        drawn.elementAt(i + 5),
+        drawn.elementAt(i + 10),
+        drawn.elementAt(i + 15),
+        drawn.elementAt(i + 20),
+      ];
+
+      if (column.where((element) => element).isNotEmpty) return true;
+    }
+
+    return false;
+  }
+
+  /// Returns true if this number leads to a winning board.
+  bool drawNumber(int number) {
+    drawn[board.indexOf(number)] = true;
+    return checkVictory();
+  }
+}
+
 void main(List<String> args) {
   final path = args.isNotEmpty ? args[0] : 'day04/day04.txt';
   final rawData = File(path).readAsLinesSync();
@@ -17,6 +62,8 @@ void main(List<String> args) {
     }
     boards.add(board);
   }
+
+  // Play bingo
 
   print(boards.length);
 }
