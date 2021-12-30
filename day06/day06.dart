0c9d3f840a9502fd1b59d86a1bf12dbf36f52c9f
Tim Sneath <tim@sneath.org>
2021-12-29 23:01:08 -0800

    Day 6 solution (#5)
    * Day 1 solution
    
    * Add collection for ListEquality
    
    * Switch to faster algorithm for day 2
    
    * Fix fish spawning algorithm
    
    * Add working day 2 solution

---
+++ day06/day06.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+
+class LanternFish {
+  // Number of fish at each timer level
+  List<int> fishTally;
+
+  LanternFish(this.fishTally) {
+    fishTally = List<int>.generate(
+        9, (index) => fishTally.where((element) => element == index).length);
+  }
+
+  void addDay() {
+    final spawning = fishTally.first;
+
+    // Rotate entries above 0 down by 1
+    fishTally = fishTally.skip(1).toList();
+
+    // After spawning, parent fish have a timer of 6
+    fishTally[6] += spawning; // parent
+    fishTally.add(spawning); // child
+  }
+
+  void addDays(int days) {
+    for (var idx = 0; idx < days; idx++) {
+      addDay();
+    }
+  }
+
+  int get count => fishTally.fold(0, (p, e) => p + e);
+
+  @override
+  int get hashCode => fishTally.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      other is LanternFish &&
+      ListEquality<int>().equals(fishTally, other.fishTally);
+
+  @override
+  String toString() => fishTally.toString();
+}
+
+void main(List<String> args) {
+  final path = args.isNotEmpty ? args[0] : 'day06/day06.txt';
+  final rawData = File(path).readAsLinesSync();
+
+  final fishData = rawData[0].split(',').map<int>((e) => int.parse(e)).toList();
+
+  final lanternFish = LanternFish(fishData);
+  lanternFish.addDays(80);
+  print('After 80 days, there are ${lanternFish.count} fish.');
+
+  final moreLanternFish = LanternFish(fishData);
+  moreLanternFish.addDays(256);
+  print('After 256 days, there are ${moreLanternFish.count} fish.');
+}
---
+++ test/day06_test.dart
@@ -0,0 +1,74 @@
+import 'package:test/test.dart';
+
+import '../day06/day06.dart';
+
+void main() {
+  test('Convert lists of fish into tally represenation', () {
+    final fish = LanternFish([3]);
+    expect(fish.fishTally, equals([0, 0, 0, 1, 0, 0, 0, 0, 0]));
+  });
+
+  test('Follow fish through days', () {
+    final fish = LanternFish([3]);
+    expect(fish.fishTally, equals([0, 0, 0, 1, 0, 0, 0, 0, 0]));
+    fish.addDay();
+    expect(fish.fishTally, equals([0, 0, 1, 0, 0, 0, 0, 0, 0]));
+    fish.addDay();
+    expect(fish.fishTally, equals([0, 1, 0, 0, 0, 0, 0, 0, 0]));
+    fish.addDay();
+    expect(fish.fishTally, equals([1, 0, 0, 0, 0, 0, 0, 0, 0]));
+    fish.addDay();
+    expect(fish.fishTally, equals([0, 0, 0, 0, 0, 0, 1, 0, 1]));
+    fish.addDay();
+    expect(fish.fishTally, equals([0, 0, 0, 0, 0, 1, 0, 1, 0]));
+  });
+
+  test('Sample after multiple days', () {
+    final fish = LanternFish([3, 4, 3, 1, 2]);
+
+    fish.addDay(); // day 1
+    expect(fish, equals(LanternFish([2, 3, 2, 0, 1])));
+
+    fish.addDay(); // day 2
+    expect(fish, equals(LanternFish([1, 2, 1, 6, 0, 8])));
+
+    fish.addDay(); // day 3
+    expect(fish, equals(LanternFish([0, 1, 0, 5, 6, 7, 8])));
+
+    fish.addDay(); // day 4
+    expect(fish, equals(LanternFish([6, 0, 6, 4, 5, 6, 7, 8, 8])));
+
+    fish.addDay(); // day 5
+    expect(fish, equals(LanternFish([5, 6, 5, 3, 4, 5, 6, 7, 7, 8])));
+
+    fish.addDay(); // day 6
+    expect(fish, equals(LanternFish([4, 5, 4, 2, 3, 4, 5, 6, 6, 7])));
+
+    fish.addDay(); // day 7
+    expect(fish, equals(LanternFish([3, 4, 3, 1, 2, 3, 4, 5, 5, 6])));
+  });
+
+  test('Sample after 18 days', () {
+    final fish = LanternFish([3, 4, 3, 1, 2]);
+    fish.addDays(18);
+
+    // fishList == 6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1,
+    //             1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8
+    expect(fish.fishTally, equals([3, 5, 3, 2, 2, 1, 5, 1, 4]));
+    expect(fish.count, equals(26));
+  });
+
+  test('Sample after 80 days', () {
+    final fish = LanternFish([3, 4, 3, 1, 2]);
+    fish.addDays(80);
+
+    expect(fish.count, equals(5934));
+  });
+
+  test('Sample after 256 days', () {
+    final fish = LanternFish([3, 4, 3, 1, 2]);
+    fish.addDays(256);
+
+    expect(fish.count, equals(26984457539));
+  });
+}
