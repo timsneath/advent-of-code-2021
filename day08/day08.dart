9298c6b1e011a1c7f7825c40bd2642465d931585
Tim Sneath <tim@sneath.org>
2021-12-31 13:25:10 -0800

    Part 1

--- day08/day08.dart
+++ day08/day08.dart
@@ -5,19 +5,32 @@ class Signal {
   final List<String> output;
 
   const Signal(this.input, this.output);
+
+  int get uniqueSegments {
+    return output
+        .where((element) => [
+              2, // one digit
+              4, // four digit
+              3, // seven digit
+              7, // eight digit
+            ].contains(element.length))
+        .length;
+  }
+
+  factory Signal.fromString(String raw) {
+    final splitRow = raw.split(' | ');
+    final input = splitRow.first.split(' ');
+    final output = splitRow.last.split(' ');
+    return Signal(input, output);
+  }
 }
 
 void main(List<String> args) {
   final path = args.isNotEmpty ? args[0] : 'day08/day08.txt';
   final rawData = File(path).readAsLinesSync();
-  final signals = <Signal>[];
+  final signals = rawData.map((e) => Signal.fromString(e));
 
-  for (final row in rawData) {
-    final splitRow = row.split(' | ');
-    final input = splitRow.first.split(' ');
-    final output = splitRow.last.split(' ');
-    signals.add(Signal(input, output));
-  }
-
-  print(signals.length);
+  final uniqueSegments =
+      signals.map((e) => e.uniqueSegments).reduce((v, e) => v + e);
+  print('Unique segments in output: $uniqueSegments');
 }
--- test/day08_test.dart
+++ test/day08_test.dart
@@ -2,4 +2,37 @@ import 'package:test/test.dart';
 
 import '../day08/day08.dart';
 
-void main() {}
+const rawData = '''
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce''';
+
+void main() {
+  test('Load raw data', () {
+    final signals = rawData.split('\n').map((e) => Signal.fromString(e));
+    expect(signals.length, equals(10));
+    expect(signals.first.input.length, equals(10));
+    expect(signals.first.output.length, equals(4));
+  });
+
+  test('Unique segments (first)', () {
+    final signals = rawData.split('\n').map((e) => Signal.fromString(e));
+    expect(signals.first.uniqueSegments, equals(2));
+    expect(signals.first.input.length, equals(10));
+    expect(signals.first.output.length, equals(4));
+  });
+
+  test('Unique segments', () {
+    final signals = rawData.split('\n').map((e) => Signal.fromString(e));
+    final uniqueSegments =
+        signals.map((e) => e.uniqueSegments).reduce((v, e) => v + e);
+    expect(uniqueSegments, equals(26));
+  });
+}
