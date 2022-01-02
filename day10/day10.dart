ad20cd0e1130522aea51d15ad71930435974a62a
Tim Sneath <tim@sneath.org>
2022-01-02 11:14:11 -0800

    Part 1

--- day10/day10.dart
+++ day10/day10.dart
@@ -1,4 +1,8 @@
+import 'dart:collection';
 import 'dart:io';
+
+const openBrackets = ['(', '[', '{', '<'];
+const closeBrackets = [')', ']', '}', '>'];
 
 const illegalCharacterScores = <String, int>{
   ')': 3,
@@ -12,8 +16,29 @@ class SyntaxChecker {
 
   SyntaxChecker(this.data);
 
-  int calculateTotalScore() {
+  int scoreSyntaxErrors(String line) {
+    final stack = ListQueue<String>();
+    for (final char in line.split('')) {
+      final bracket = openBrackets.indexOf(char);
+      if (bracket != -1) {
+        // Bracket found, so add matching close bracket to the stack
+        stack.addLast(closeBrackets[bracket]);
+      } else {
+        final expected = stack.removeLast();
+        if (char != expected) {
+          // Line is corrupted, so return score for unexpected character
+          return illegalCharacterScores[char]!;
+        }
+      }
+    }
     return 0;
+  }
+
+  bool hasSyntaxError(String line) => scoreSyntaxErrors(line) > 0;
+
+  int calculateTotalScore() {
+    final scores = data.map(scoreSyntaxErrors);
+    return scores.reduce((value, element) => value + element);
   }
 }
 
--- test/day10_test.dart
+++ test/day10_test.dart
@@ -16,6 +16,15 @@ const rawData = <String>[
 ];
 
 void main() {
+  test('Expected number of syntax errors', () {
+    final syntaxChecker = SyntaxChecker(rawData);
+    expect(
+        syntaxChecker.data
+            .where((row) => syntaxChecker.hasSyntaxError(row))
+            .length,
+        equals(5));
+  });
+
   test('Total error syntax score', () {
     final syntaxChecker = SyntaxChecker(rawData);
     expect(syntaxChecker.calculateTotalScore(), equals(26397));
