77bee1214137861a3c6606c6bf59e0b02432d869
Tim Sneath <tim@sneath.org>
2022-01-02 11:27:16 -0800

    Minor cleanup

--- test/day03_test.dart
+++ test/day03_test.dart
@@ -2,6 +2,21 @@ import 'package:test/test.dart';
 
 import '../day03/day03.dart';
 
+const sampleInput = <String>[
+  '00100',
+  '11110',
+  '10110',
+  '10111',
+  '10101',
+  '01111',
+  '00111',
+  '11100',
+  '10000',
+  '11001',
+  '00010',
+  '01010',
+];
+
 void main() {
   group('Part 1', () {
     test('Simple example', () {
@@ -11,62 +26,25 @@ void main() {
     });
 
     test('Sample input', () {
-      final input = <String>[
-        '00100',
-        '11110',
-        '10110',
-        '10111',
-        '10101',
-        '01111',
-        '00111',
-        '11100',
-        '10000',
-        '11001',
-        '00010',
-        '01010'
-      ];
-      final power = calcPowerConsumption(convertRawData(input));
+      final power = calcPowerConsumption(convertRawData(sampleInput));
       expect(power, equals(198));
     });
   });
 
   group('Part 2', () {
     test('Oxygen generator rating', () {
-      final input = <String>[
-        '00100',
-        '11110',
-        '10110',
-        '10111',
-        '10101',
-        '01111',
-        '00111',
-        '11100',
-        '10000',
-        '11001',
-        '00010',
-        '01010'
-      ];
-      final o2 = calcOxygenGeneratorRating(convertRawData(input));
+      final o2 = calcOxygenGeneratorRating(convertRawData(sampleInput));
       expect(o2, equals(23));
     });
 
     test('CO2 scrubber rating', () {
-      final input = <String>[
-        '00100',
-        '11110',
-        '10110',
-        '10111',
-        '10101',
-        '01111',
-        '00111',
-        '11100',
-        '10000',
-        '11001',
-        '00010',
-        '01010'
-      ];
-      final co2 = calcCO2ScrubberRating(convertRawData(input));
+      final co2 = calcCO2ScrubberRating(convertRawData(sampleInput));
       expect(co2, equals(10));
+    });
+
+    test('Life support rating', () {
+      final lifeSupport = calcLifeSupportRating(convertRawData(sampleInput));
+      expect(lifeSupport, equals(230));
     });
   });
 }
