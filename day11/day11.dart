23a9f5e5518c779a7003235590d5a228199f7ba4
Tim Sneath <tim@sneath.org>
2022-01-02 15:37:52 -0800

    Day 11 solution (#10)
    * Baseline
    
    * Broken part 1
    
    * Working part 2

---
+++ day11/day11.dart
@@ -0,0 +1,124 @@
+import 'dart:io';
+
+class Cavern {
+  late final List<List<int>> grid;
+  int flashCount = 0;
+
+  Cavern(this.grid);
+
+  factory Cavern.fromRawData(List<String> rawData) {
+    final grid = <List<int>>[];
+    for (final row in rawData) {
+      grid.add(row.split('').map((c) => int.parse(c)).toList());
+    }
+    return Cavern(grid);
+  }
+
+  @override
+  String toString() => grid.map((row) => row.join('')).join('\n');
+
+  int get height => grid.length;
+  int get width => grid.first.length;
+
+  int at(int x, int y) => grid[y][x];
+  void inc(int x, int y) => grid[y][x]++;
+  void reset(int x, int y) => grid[y][x] = 0;
+
+  bool aboutToFlash(int x, int y) => grid[y][x] > 9;
+  void markFlashed(int x, int y) => grid[y][x] = -1;
+  bool hasFlashed(int x, int y) => grid[y][x] == -1;
+
+  bool notInGrid(int x, int y) =>
+      (x < 0 || x >= width) || (y < 0 || y >= height);
+
+  bool get isSynchronizedFlash {
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        if (at(x, y) != 0) return false;
+      }
+    }
+    return true;
+  }
+
+  void completeDay() {
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        if (hasFlashed(x, y)) reset(x, y);
+      }
+    }
+  }
+
+  void incrementAll() {
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        inc(x, y);
+      }
+    }
+  }
+
+  void addDay() {
+    incrementAll();
+    int startCount;
+    // Keep repeating until we've covered all flashes
+    do {
+      startCount = flashCount;
+      for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+          if (at(x, y) > 9) {
+            flash(x, y);
+          }
+        }
+      }
+    } while (startCount != flashCount);
+    completeDay();
+  }
+
+  void addDays(int days) {
+    for (int x = days; x > 0; x--) {
+      addDay();
+    }
+  }
+
+  int firstSynchronizedFlash() {
+    int dayCount = 0;
+    do {
+      addDay();
+      dayCount++;
+    } while (!isSynchronizedFlash);
+    return dayCount;
+  }
+
+  void flash(int x, int y) {
+    if (notInGrid(x, y) || hasFlashed(x, y)) return;
+
+    if (aboutToFlash(x, y)) {
+      markFlashed(x, y);
+      flashCount++;
+
+      flash(x - 1, y);
+      flash(x - 1, y - 1);
+      flash(x, y - 1);
+      flash(x + 1, y - 1);
+      flash(x + 1, y);
+      flash(x + 1, y + 1);
+      flash(x, y + 1);
+      flash(x - 1, y + 1);
+    } else {
+      inc(x, y);
+    }
+  }
+}
+
+// coverage:ignore-start
+void main(List<String> args) {
+  final path = args.isNotEmpty ? args[0] : 'day11/day11.txt';
+  final rawData = File(path).readAsLinesSync();
+  final cavern = Cavern.fromRawData(rawData);
+  cavern.addDays(100);
+  print('There have been ${cavern.flashCount} flashes.');
+
+  final cavern2 = Cavern.fromRawData(rawData);
+  final firstSyncEvent = cavern2.firstSynchronizedFlash();
+  print('First synchronization event is after $firstSyncEvent days');
+}
+// coverage:ignore-end
---
+++ test/day11_test.dart
@@ -0,0 +1,156 @@
+import 'package:test/test.dart';
+
+import '../day11/day11.dart';
+
+const simpleData = <String>[
+  '00000',
+  '00000',
+  '00100',
+  '00000',
+  '00000',
+];
+
+const readyToFlashData = <String>[
+  '11111',
+  '19991',
+  '19191',
+  '19991',
+  '11111',
+];
+
+const sampleData = <String>[
+  '5483143223',
+  '2745854711',
+  '5264556173',
+  '6141336146',
+  '6357385478',
+  '4167524645',
+  '2176841721',
+  '6882881134',
+  '4846848554',
+  '5283751526',
+];
+
+void main() {
+  test('toString()', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    expect(cavern.toString(), startsWith('5483143223\n2745854711'));
+  });
+
+  test('Simple example', () {
+    final cavern = Cavern.fromRawData(simpleData);
+    cavern.addDay();
+    expect(cavern.toString(), equals('''
+11111
+11111
+11211
+11111
+11111'''));
+  });
+
+  test('Adjacent flashes', () {
+    final cavern = Cavern.fromRawData(readyToFlashData);
+    cavern.addDay();
+    expect(cavern.toString(), equals('''
+34543
+40004
+50005
+40004
+34543'''));
+
+    cavern.addDay();
+    expect(cavern.toString(), equals('''
+45654
+51115
+61116
+51115
+45654'''));
+  });
+
+  test('day 1', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    cavern.addDay();
+    expect(cavern.toString(), equals('''
+6594254334
+3856965822
+6375667284
+7252447257
+7468496589
+5278635756
+3287952832
+7993992245
+5957959665
+6394862637'''));
+  });
+
+  test('day 2', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    cavern.addDays(2);
+    expect(cavern.toString(), equals('''
+8807476555
+5089087054
+8597889608
+8485769600
+8700908800
+6600088989
+6800005943
+0000007456
+9000000876
+8700006848'''));
+  });
+
+  test('day 3', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    cavern.addDays(3);
+    expect(cavern.toString(), equals('''
+0050900866
+8500800575
+9900000039
+9700000041
+9935080063
+7712300000
+7911250009
+2211130000
+0421125000
+0021119000'''));
+  });
+
+  test('day 10', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    cavern.addDays(10);
+    expect(cavern.toString(), equals('''
+0481112976
+0031112009
+0041112504
+0081111406
+0099111306
+0093511233
+0442361130
+5532252350
+0532250600
+0032240000'''));
+  });
+
+  test('day 10 flashcount', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    cavern.addDays(10);
+    expect(cavern.flashCount, equals(204));
+  });
+
+  test('day 100 flashcount', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    cavern.addDays(100);
+    expect(cavern.flashCount, equals(1656));
+  });
+
+  test('day 195 flashes', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    cavern.addDays(195);
+    expect(cavern.grid, everyElement(<int>[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+  });
+
+  test('first synchronized count', () {
+    final cavern = Cavern.fromRawData(sampleData);
+    expect(cavern.firstSynchronizedFlash(), equals(195));
+  });
+}
