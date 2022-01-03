7e94b05d7403e267333a35c01d4a7f33bf5107bb
Tim Sneath <tim@sneath.org>
2022-01-03 12:59:11 -0800

    Part 2 complete

--- day12/day12.dart
+++ day12/day12.dart
@@ -34,27 +34,50 @@ class CaveMap {
   @override
   String toString() => edges.join('\n');
 
-  Iterable<Edge> get startEdges => edges.where((edge) => edge.from == 'start');
+  List<int> countDistribution(List<String> items) {
+    final uniqueItems = items.toSet();
+
+    final distribution = uniqueItems
+        .map((unique) => items.where((item) => item == unique).length)
+        .toList();
+    return distribution;
+  }
 
   /// Test to see whether the next path step is valid.
   ///
   /// A path is valid if the next node is a large cave (i.e. upper case), or if
-  /// the next node has been visited no more than `smallCaveMaxVisits` times.
-  bool isValidPath(String nextNode, List<String> currentPath) =>
-      nextNode.isUpperCase ||
-      currentPath.where((n) => n == nextNode).length < smallCaveMaxVisits;
+  /// the next node has been visited no more than `smallCaveMaxVisits` times AND
+  /// no more than one small cave has been visited more than once.
+  bool isValidPath(List<String> currentPath, String nextNode) {
+    if (nextNode.isUpperCase) return true;
+    if (nextNode == 'start') return false;
 
-  Iterable<List<String>> findFrom(Edge startEdge, List<String> currentPath) {
-    final paths = <List<String>>[];
+    final smallCaves = [...currentPath, nextNode]
+        .skip(1)
+        .where((cave) => cave.isLowerCase)
+        .toList();
+    final distribution = countDistribution(smallCaves);
+
+    if (smallCaveMaxVisits == 1) {
+      // OK only if none are more than 1
+      return distribution.where((element) => element > 1).isEmpty;
+    } else {
+      return distribution.where((v) => v == smallCaveMaxVisits).length <= 1 &&
+          distribution.where((v) => v > smallCaveMaxVisits).isEmpty;
+    }
+  }
+
+  Set<List<String>> findFrom(Edge startEdge, List<String> currentPath) {
+    final paths = <List<String>>{};
     final nextEdges = edges
         .where((nextEdge) => startEdge.to == nextEdge.from)
-        .where((nextEdge) => isValidPath(nextEdge.to, currentPath));
+        .where((nextEdge) => isValidPath(currentPath, nextEdge.to));
 
     for (final edge in nextEdges) {
       if (edge.to == 'end') {
         // this is a good route to the end
         paths.add([...currentPath, 'end']);
-      } else if (edge.to != 'start') {
+      } else {
         paths.addAll(findFrom(edge, [...currentPath, edge.to]));
       }
     }
@@ -62,17 +85,23 @@ class CaveMap {
   }
 
   // Returns a list of comma-separated paths from start to end
-  Iterable<String> findPaths() =>
-      findFrom(Edge(null, 'start'), ['start']).map((path) => path.join(','));
+  Set<String> findPaths() => findFrom(Edge(null, 'start'), ['start'])
+      .map((path) => path.join(','))
+      .toSet();
 }
 
 // coverage:ignore-start
 void main(List<String> args) {
   final path = args.isNotEmpty ? args[0] : 'day12/day12.txt';
   final rawData = File(path).readAsLinesSync();
-  final caveMap = CaveMap.fromRawData(rawData, 1);
 
+  final caveMap = CaveMap.fromRawData(rawData, 1);
   final paths = caveMap.findPaths();
   print('Number of paths if small cave can be visited once: ${paths.length}');
+
+  final caveMap2 = CaveMap.fromRawData(rawData, 2);
+  final paths2 = caveMap2.findPaths();
+  print(
+      'Number of paths if one small cave can be visited twice: ${paths2.length}');
 }
 // coverage:ignore-end
