11fb70264dc32c3a56f99b61b0925ff15eed27ce
Tim Sneath <timsneath@google.com>
2022-01-04 10:24:57 -0800

    Tidy early days to established format

--- test/day01_test.dart
+++ test/day01_test.dart
@@ -4,67 +4,65 @@ import '../day01/day01.dart';
 
 void main() {
   group('Part 1', () {
-    test('Empty stream', () async {
-      final depths = Stream.fromIterable(<int>[]);
-      final increases = await tallyDepthIncreases(depths);
+    test('Empty stream', () {
+      final depths = <int>[];
+      final increases = tallyDepthIncreases(depths);
       expect(increases, equals(0));
     });
 
-    test('Increasing depths', () async {
-      final depths = Stream.fromIterable([0, 1, 2]);
-      final increases = await tallyDepthIncreases(depths);
+    test('Increasing depths', () {
+      final depths = <int>[0, 1, 2];
+      final increases = tallyDepthIncreases(depths);
       expect(increases, equals(2));
     });
 
-    test('Decreasing depths', () async {
-      final depths = Stream.fromIterable([2, 1, 0]);
-      final increases = await tallyDepthIncreases(depths);
+    test('Decreasing depths', () {
+      final depths = <int>[2, 1, 0];
+      final increases = tallyDepthIncreases(depths);
       expect(increases, equals(0));
     });
 
-    test('Descend and increase', () async {
-      final depths = Stream.fromIterable([1, 2, 3, 2, 1]);
-      final increases = await tallyDepthIncreases(depths);
+    test('Descend and increase', () {
+      final depths = <int>[1, 2, 3, 2, 1];
+      final increases = tallyDepthIncreases(depths);
       expect(increases, equals(2));
     });
 
-    test('Given test', () async {
-      final depths = Stream.fromIterable(
-          [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
-      final increases = await tallyDepthIncreases(depths);
+    test('Given test', () {
+      final depths = <int>[199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+      final increases = tallyDepthIncreases(depths);
       expect(increases, equals(7));
     });
   });
 
   group('Part 2', () {
-    test('Empty stream', () async {
-      final depths = Stream.fromIterable(<int>[]);
-      final increases = await tallySlidingWindowDepthIncreases(depths);
+    test('Empty stream', () {
+      final depths = <int>[];
+      final increases = tallySlidingWindowDepthIncreases(depths);
       expect(increases, equals(0));
     });
 
-    test('Increasing depths', () async {
-      final depths = Stream.fromIterable([0, 1, 2, 3, 4, 5, 6, 7]);
-      final increases = await tallySlidingWindowDepthIncreases(depths);
+    test('Increasing depths', () {
+      final depths = [0, 1, 2, 3, 4, 5, 6, 7];
+      final increases = tallySlidingWindowDepthIncreases(depths);
       expect(increases, equals(5));
     });
 
-    test('Decreasing depths', () async {
-      final depths = Stream.fromIterable([7, 6, 5, 4, 3, 2, 1, 0]);
-      final increases = await tallySlidingWindowDepthIncreases(depths);
+    test('Decreasing depths', () {
+      final depths = [7, 6, 5, 4, 3, 2, 1, 0];
+      final increases = tallySlidingWindowDepthIncreases(depths);
       expect(increases, equals(0));
     });
 
-    test('Descend and increase', () async {
-      final depths = Stream.fromIterable([1, 2, 3, 4, 3, 2, 1]);
-      final increases = await tallySlidingWindowDepthIncreases(depths);
+    test('Descend and increase', () {
+      final depths = [1, 2, 3, 4, 3, 2, 1];
+      final increases = tallySlidingWindowDepthIncreases(depths);
       expect(increases, equals(2));
     });
 
-    test('Given test', () async {
-      final depths = Stream.fromIterable(
-          [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
-      final increases = await tallySlidingWindowDepthIncreases(depths);
+    test('Given test', () {
+      final depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+      final increases = tallySlidingWindowDepthIncreases(depths);
       expect(increases, equals(5));
     });
   });
--- test/day02_test.dart
+++ test/day02_test.dart
@@ -4,31 +4,31 @@ import '../day02/day02.dart';
 
 void main() {
   group('Part 1', () {
-    test('Sample input', () async {
-      final depths = Stream.fromIterable(<String>[
+    test('Sample input', () {
+      final depths = <String>[
         'forward 5',
         'down 5',
         'forward 8',
         'up 3',
         'down 8',
         'forward 2'
-      ]);
-      final courseDistance = await plotCourse(depths);
+      ];
+      final courseDistance = plotCourse(depths);
       expect(courseDistance, equals(150));
     });
   });
 
   group('Part 2', () {
     test('Sample input', () async {
-      final depths = Stream.fromIterable(<String>[
+      final depths = <String>[
         'forward 5',
         'down 5',
         'forward 8',
         'up 3',
         'down 8',
         'forward 2'
-      ]);
-      final courseDistance = await updatedPlotCourse(depths);
+      ];
+      final courseDistance = updatedPlotCourse(depths);
       expect(courseDistance, equals(900));
     });
   });
